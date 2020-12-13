9b0b2b25f79d6d93e91aaa4b771c785997cb1d23
cxinping <xpws2006@163.com>
2020-12-13 21:29:30 +0800

    no message

--- bigdata-study/实验例子/gunicorndemo/demo/Test1.py
+++ bigdata-study/实验例子/gunicorndemo/demo/Test1.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
 
-HOST = '192.168.11.10'
-PORT = '3306'
-USERNAME = 'xinping'
-PASSWORD = '123@Welcome'
-DATABASE = 'RAD1'
-DB_URI = 'mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset-utf8'.format(username=USERNAME, password=PASSWORD, host=HOST,  port=PORT,                      db=DATABASE)
-
-print(DB_URI)
