edc922fb8a5c520f90174ddb65006c054d9378ac
cxinping <xpws2006@163.com>
2021-10-03 16:39:14 +0800

    1

--- Flink2/flink_course2/src/main/java/com/dxtd/app/TupleApp.java
+++ Flink2/flink_course2/src/main/java/com/dxtd/app/TupleApp.java
@@ -15,17 +15,15 @@ public class TupleApp {
         tupleTest();
         //mapTest();
         //flatMapTest();
-
     }
 
     private static void tupleTest(){
-        Tuple3<Integer,String,Long> tuple3 =  Tuple3.of(1,"xdclass.net",120L);
+        Tuple3<Integer,String,Long> tuple3 =  Tuple3.of(1,"ultrapower.com",130L);
 
         System.out.println(tuple3.f0);
         System.out.println(tuple3.f1);
         System.out.println(tuple3.f2);
     }
-
 
     private static void mapTest(){
         List<String> list1 = new ArrayList<>();
@@ -34,15 +32,12 @@ public class TupleApp {
         list1.add("kafka,rabbitmq");
 
         List<String> result = list1.stream().map(obj->{
-
             obj = "小滴课堂"+obj;
             return  obj;
         }).collect(Collectors.toList());
 
         System.out.println(result);
-
     }
-
 
     private static void flatMapTest(){
         List<String> list1 = new ArrayList<>();
@@ -50,16 +45,12 @@ public class TupleApp {
         list1.add("redis6,docker");
         list1.add("kafka,rabbitmq");
 
-
         List<String> result =  list1.stream().flatMap(obj->{
             Stream<String> stream =  Arrays.stream( obj.split(","));
             return stream;
         }).collect(Collectors.toList());
 
         System.out.println(result);
-
-
-
     }
 
 
