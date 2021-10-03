7acbbba9ccd14e08663bf3ffde7ece95666da5ec
cxinping <xpws2006@163.com>
2021-10-03 11:14:45 +0800

    测试

---
+++ Flink2/flink_course1/src/main/java/com/dxtd/model/ProductOrder.java
@@ -0,0 +1,18 @@
+package com.dxtd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductOrder {
+    private int id;
+
+    private String title;
+
+    private Long amount;
+
+
+}
