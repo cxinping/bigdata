033dd1b8dfa28d87d37c410fce6fcdfac90fff97
cxinping <xpws2006@163.com>
2021-10-09 10:40:36 +0800

    1

--- bigdata-report/report/commons/connect_kudu.py
+++ bigdata-report/report/commons/connect_kudu.py
@@ -109,7 +109,7 @@ def prod_execute_sql(conn_type='prod', sqltype='insert', sql=''):
     TEST = 'test'  # 测试环境
 
     print('**** prod_execute_sql ****')
-    print('---- conn_type=', conn_type)
+    print('* conn_type=', conn_type)
 
     if conn_type == PROD:
         # 生产集群使用KUDU
