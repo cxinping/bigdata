a16d5a2e680f4219d106d813a97814fc09522a6e
cxinping <xpws2006@163.com>
2021-10-12 14:38:53 +0800

    1

--- bigdata-report/report/works/execte_02_data.py
+++ bigdata-report/report/works/execte_02_data.py
@@ -1,1 +1,86 @@
 # -*- coding: utf-8 -*-
+from report.commons.logging import get_logger
+from report.commons.connect_kudu import prod_execute_sql, dis_connection
+import time
+import os
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+log = get_logger(__name__)
+
+
+def execute_02_data():
+    columns_ls = ['destin_name', 'sales_name', 'sales_addressphone', 'sales_bank']
+    extra_columns_ls = ['bill_id']
+    columns_ls.extend(extra_columns_ls)
+    columns_str = ",".join(columns_ls)
+    sql = """
+    select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null ) 
+    limit 10000
+    """.format(columns_str=columns_str).replace('\n', '').replace('\r', '').strip()
+
+    log.info(sql)
+    count_sql = 'select count(a.bill_id) from ({sql}) a'.format(sql=sql)
+    log.info(count_sql)
+    records = prod_execute_sql(conn_type='test', sqltype='select', sql=count_sql)
+    count_records = records[0][0]
+
+    max_size = 1 * 10000
+    limit_size = 5000
+    select_sql_ls = []
+
+    log.info(f'* count_records ==> {count_records}')
+    if count_records >= max_size:
+        offset_size = 0
+        while offset_size <= count_records:
+            if offset_size + limit_size > count_records:
+                limit_size = count_records - offset_size
+                tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null )  order by finance_travel_id limit {limit_size} offset {offset_size}".format(
+                    columns_str=columns_str, limit_size=limit_size, offset_size=offset_size)
+
+                select_sql_ls.append(tmp_sql)
+                break
+            else:
+                tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null )  order by finance_travel_id limit {limit_size} offset {offset_size}".format(
+                    columns_str=columns_str, limit_size=limit_size, offset_size=offset_size)
+                select_sql_ls.append(tmp_sql)
+
+            offset_size = offset_size + limit_size
+    else:
+        tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null ) ".format(
+            columns_str=columns_str)
+        select_sql_ls.append(tmp_sql)
+        print('*** tmp_sql => ', tmp_sql)
+
+    log.info('* 开始分页查询')
+
+    obj_list = []
+    threadPool = ThreadPoolExecutor(max_workers=20)
+    start_time = time.perf_counter()
+
+    for sel_sql in select_sql_ls:
+        log.info(sel_sql)
+        obj = threadPool.submit(exec_task, sel_sql, columns_ls)
+        obj_list.append(obj)
+
+    for future in as_completed(obj_list):
+        data = future.result()
+        if data:
+            #print(data)
+            pass
+
+
+    threadPool.shutdown(wait=True)
+    consumed_time = round(time.perf_counter() - start_time)
+    log.info(f'* 查询耗时 {consumed_time} sec')
+
+def exec_task(sql, columns_ls):
+    records = prod_execute_sql(conn_type='test', sqltype='select', sql=sql)
+    if len(records) > 0:
+        return records
+    else:
+        return None
+
+execute_02_data()
+print('--- ok ---')
+os._exit(0)  # 无错误退出
