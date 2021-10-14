2a7dec4506f70ab08ad63c117805d80fc767840b
cxinping <xpws2006@163.com>
2021-10-14 16:50:28 +0800

    1

--- bigdata-report/report/works/exec_55_dta.py
+++ bigdata-report/report/works/exec_55_dta.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+from report.commons.logging import get_logger
+from report.commons.connect_kudu import prod_execute_sql, dis_connection
+import time
+import pandas as pd
+from report.commons.db_helper import query_kudu_data
+from report.commons.tools import list_of_groups
+
+log = get_logger(__name__)
+
+import sys
+sys.path.append('/you_filed_algos/app')
+
+
+def exec_55_data():
+    pass
+
+
+
+
+
+exec_55_data()
+
+
