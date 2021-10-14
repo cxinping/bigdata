d47b41b26aa69937c67406cee14070c3ea1211cd
cxinping <xpws2006@163.com>
2021-10-14 14:35:59 +0800

    2

--- bigdata-report/report/algorithm/check_13_data.py
+++ bigdata-report/report/algorithm/check_13_data.py
@@ -6,12 +6,13 @@ import pandas as pd
 
 log = get_logger(__name__)
 
+
 def check_13_data():
     """
     关注相同或临近期间，同一费用发生地，不同人员或企业的住宿费偏离平均值或者大多数人费用分布区间的情况。
     :return:
     """
-    columns_ls = ['bill_id', 'destin_name' , 'check_amount']
+    columns_ls = ['bill_id', 'destin_name', 'check_amount']
     columns_str = ",".join(columns_ls)
     sql = 'select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null'.format(
         columns_str=columns_str)
@@ -23,11 +24,7 @@ def check_13_data():
     log.info(f'* count_records ==> {count_records}')
 
     records = prod_execute_sql(conn_type='test', sqltype='select', sql=sql)
-    print('len(records) ==> ' , len(records))
-
-
-
-
+    print('len(records) ==> ', len(records))
 
 
 check_13_data()
--- bigdata-report/report/works/execte_02_data.py
+++ bigdata-report/report/works/execte_02_data.py
@@ -9,7 +9,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from report.commons.tools import match_address
 from report.commons.test_hdfs_tools import HDFSTools as Test_HDFSTools
 
-
 """
 
 把上传的数据放到 02_logical_layer_007_h_lf_cw.finance_travel_linshi_analysis 表里
@@ -21,18 +20,18 @@ dest_file = "/you_filed_algos/prod_kudu_data/check_02_trip_data.txt"
 upload_hdfs_path = '/user/hive/warehouse/02_logical_layer_007_h_lf_cw.db/finance_travel_linshi_analysis/'
 
 
-
 def init_file():
     if os.path.exists(dest_file):
         os.remove(dest_file)
 
+
 def execute_02_data():
-    columns_ls = ['destin_name', 'sales_name', 'sales_addressphone', 'sales_bank' , 'finance_travel_id']
+    columns_ls = ['destin_name', 'sales_name', 'sales_addressphone', 'sales_bank', 'finance_travel_id']
     extra_columns_ls = ['bill_id']
     columns_ls.extend(extra_columns_ls)
     columns_str = ",".join(columns_ls)
     sql = """
-    select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null ) 
+    select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where sales_name is not null or sales_addressphone is not null or sales_bank is not null 
     
     """.format(columns_str=columns_str).replace('\n', '').replace('\r', '').strip()
 
@@ -52,19 +51,19 @@ def execute_02_data():
         while offset_size <= count_records:
             if offset_size + limit_size > count_records:
                 limit_size = count_records - offset_size
-                tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null )  order by finance_travel_id limit {limit_size} offset {offset_size}".format(
+                tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where sales_name is not null or sales_addressphone is not null or sales_bank is not null order by finance_travel_id limit {limit_size} offset {offset_size}".format(
                     columns_str=columns_str, limit_size=limit_size, offset_size=offset_size)
 
                 select_sql_ls.append(tmp_sql)
                 break
             else:
-                tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null )  order by finance_travel_id limit {limit_size} offset {offset_size}".format(
+                tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where sales_name is not null or sales_addressphone is not null or sales_bank is not null order by finance_travel_id limit {limit_size} offset {offset_size}".format(
                     columns_str=columns_str, limit_size=limit_size, offset_size=offset_size)
                 select_sql_ls.append(tmp_sql)
 
             offset_size = offset_size + limit_size
     else:
-        tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where destin_name is not null and (sales_name is not null and sales_addressphone is not null and sales_bank is not null ) ".format(
+        tmp_sql = "select {columns_str} from 01_datamart_layer_007_h_cw_df.finance_travel_bill where sales_name is not null or sales_addressphone is not null or sales_bank is not null".format(
             columns_str=columns_str)
         select_sql_ls.append(tmp_sql)
         print('*** tmp_sql => ', tmp_sql)
@@ -90,7 +89,7 @@ def execute_02_data():
 
             for record in data:
                 sales_address = operate_reocrd(record)
-                #print()
+                # print()
 
                 destin_name = str(record[0])
                 sales_name = str(record[1])
@@ -110,7 +109,6 @@ def execute_02_data():
     log.info(f'* 查询耗时 {consumed_time} sec')
 
 
-
 def operate_reocrd(record):
     destin_name = str(record[0])
     sales_name = str(record[1])
@@ -126,17 +124,19 @@ def operate_reocrd(record):
 
         # 只匹配市和县
         if sales_name != 'None':
-            sales_name_city = match_address(place=sales_name, key='市') if match_address(place=sales_name,key='市') else match_address(place=sales_name, key='县')
+            sales_name_city = match_address(place=sales_name, key='市') if match_address(place=sales_name,
+                                                                                        key='市') else match_address(
+                place=sales_name, key='县')
 
             if sales_name_city:
-                #print('sales_address=', sales_name_city)
+                # print('sales_address=', sales_name_city)
                 return sales_name_city
 
         if sales_addressphone != 'None':
             sales_addressphone_city = match_address(place=sales_addressphone, key='市') if match_address(
                 place=sales_addressphone, key='市') else match_address(place=sales_addressphone, key='县')
-            if sales_addressphone_city :
-                #print('sales_address=', sales_addressphone_city)
+            if sales_addressphone_city:
+                # print('sales_address=', sales_addressphone_city)
                 return sales_addressphone_city
 
         if sales_bank != 'None':
@@ -144,7 +144,7 @@ def operate_reocrd(record):
                                                                                         key='市') else match_address(
                 place=sales_bank, key='县')
             if sales_bank_city:
-                #print('sales_address=', sales_bank_city)
+                # print('sales_address=', sales_bank_city)
                 return sales_bank_city
 
 
@@ -158,10 +158,10 @@ def exec_task(sql):
 
 def main():
     execute_02_data()
-    print('--- ok ---')
+    print('--- created txt file ---')
 
     test_hdfs = Test_HDFSTools(conn_type='test')
-    #test_hdfs.uploadFile2(hdfsDirPath=upload_hdfs_path, localPath=dest_file)
+    test_hdfs.uploadFile2(hdfsDirPath=upload_hdfs_path, localPath=dest_file)
 
     os._exit(0)  # 无错误退出
 
