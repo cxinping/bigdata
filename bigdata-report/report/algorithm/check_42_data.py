253691c8366bc0307204d760d533a93ded032978
cxinping <xpws2006@163.com>
2021-10-20 15:07:12 +0800

    好天气

--- bigdata-report/report/algorithm/check_42_data.py
+++ bigdata-report/report/algorithm/check_42_data.py
@@ -123,16 +123,126 @@ def clean_stopword(jiebaword, stopword):
     return clean_words
 
 
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.cluster.kmeans import KMeansClusterer
+from nltk.cluster.util import cosine_distance
+from collections import Counter
+
+
+def get_tfidf(clean_words):
+    """
+    生成tf-idf矩阵文档
+    :return:
+    """
+    transformer = TfidfVectorizer()
+    tfidf = transformer.fit_transform(clean_words)
+    # 转为数组形式
+    tfidf_arr = tfidf.toarray()
+    return tfidf_arr
+
+
+def get_cluster(tfidf_arr, k):
+    """
+    K-means聚类
+    :param tfidf_arr:
+    :param k:
+    :return:
+    """
+    kmeans = KMeansClusterer(num_means=k, distance=cosine_distance, avoid_empty_clusters=True)  # 分成k类，使用余弦相似分析
+    kmeans.cluster(tfidf_arr)
+
+    # 获取分类
+    kinds = pd.Series([kmeans.classify(i) for i in tfidf_arr])
+    fw = open('/you_filed_algos/prod_kudu_data/ClusterText.txt', 'a+', encoding='utf-8')
+    for i, v in kinds.items():
+        fw.write(str(i) + '\t' + str(v) + '\n')
+    fw.close()
+
+def cluster_text(clean_words):
+    """
+    获取分类文档
+    :return:
+    """
+    index_cluser = []
+    try:
+        with open('/you_filed_algos/prod_kudu_data/ClusterText.txt', "r", encoding='utf-8') as fr:
+            lines = fr.readlines()
+    except FileNotFoundError:
+        print("no file like this")
+
+    for line in lines:
+        line = line.strip('\n')
+        line = line.split('\t')
+        index_cluser.append(line)
+
+    for index, line in enumerate(clean_words):
+        for i in range(28):
+            if str(index) == index_cluser[i][0]:
+                fw = open('/you_filed_algos/prod_kudu_data/cluster' + index_cluser[i][1] + '.txt', 'a+', encoding='utf-8')
+                fw.write(line)
+    fw.close()
+
+
+def get_title(cluster):
+    """
+     获取主题词
+    :param cluster:
+    :return:
+    """
+    for i in range(cluster):
+        try:
+            with open('/you_filed_algos/prod_kudu_data/cluster' + str(i) + '.txt', "r", encoding='utf-8') as fr:
+                lines = fr.readlines()
+        except FileNotFoundError:
+            print("no file like this")
+        all_words = []
+        for line in lines:
+            line = line.strip('\n')
+            line = line.split('\t')
+            for word in line:
+                all_words.append(word)
+        c = Counter()
+        for x in all_words:
+            if len(x) > 1 and x != '\r\n':
+                c[x] += 1
+
+        print('主题' + str(i+1) + '\n词频统计结果：')
+        # 输出词频最高的那个词，也可以输出多个高频词
+        print('=================================')
+        for (k, v) in c.most_common(1):
+            print(k,':',v,'\n')
+
+
+
+
 def main():
     # records_ls = execute_sql()
     # clustering(records_ls)
 
     jiebaword = get_jiebaword()
     stopword = get_custom_stopwords("/you_filed_algos/app/report/algorithm/stop_words.txt")
-    # print(stopword)
+    print('stopword => ',stopword)
     clean_words = clean_stopword(jiebaword, stopword)
-    print(clean_words)
+    print('clean_words => ',clean_words)
+    tfidf_arr = get_tfidf(clean_words)
+
+    # print(tfidf_arr)
+    # print(tfidf_arr.shape)
+
+    # 定义聚类的个数
+    # cluster = 3
+    # # K-means聚类
+    # get_cluster(tfidf_arr, cluster)
+    #
+    # # 获取分类文件
+    # cluster_text(clean_words)
+    #
+    # # 统计出主题词
+    # get_title(cluster)
+
+
+
 
 
 main()
-
