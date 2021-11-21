57a9fa7a9e6323e8bea83b3d50b540cd284b7511
cxinping <xpws2006@163.com>
2021-11-21 12:00:08 +0800

    no message

---
+++ kafka_study/kafka_tutorials/src/main/java/com/kafka/tutorials/kafka_tutorials/Test.java
@@ -0,0 +1,8 @@
+package com.kafka.tutorials.kafka_tutorials;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ kafka_study/kafka_tutorials/src/test/java/com/kafka/tutorials/kafka_tutorials/KafkaTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.tutorials.kafka_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
