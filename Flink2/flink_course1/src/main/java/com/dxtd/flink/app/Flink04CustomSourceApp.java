620d72abce5ddeb103f664cf1f3eb6abf072c46d
cxinping <xpws2006@163.com>
2022-01-29 01:30:49 +0800

    no message

---
+++ Flink2/flink_course1/src/main/java/com/dxtd/flink/app/Flink04CustomSourceApp.java
@@ -0,0 +1,43 @@
+//package com.dxtd.flink.app;
+//
+//import com.dxtd.flink.model.VideoOrder;
+//import com.dxtd.flink.source.VideoOrderSource;
+//import org.apache.flink.api.common.RuntimeExecutionMode;
+//import org.apache.flink.api.common.functions.FilterFunction;
+//import org.apache.flink.configuration.Configuration;
+//import org.apache.flink.streaming.api.datastream.DataStream;
+//import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+//
+//public class Flink04CustomSourceApp {
+//
+//    /**
+//     * source
+//     * transformation
+//     * sink
+//     *
+//     * @param args
+//     */
+//    public static void main(String[] args) throws Exception {
+//
+//        //构建执行任务环境以及任务的启动的入口, 存储全局相关的参数
+//        //StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+//        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
+//        env.setParallelism(2);
+//
+//        DataStream<VideoOrder> videoOrderDS =  env.addSource(new VideoOrderSource());
+//
+//        DataStream<VideoOrder> filterDS = videoOrderDS.filter(new FilterFunction<VideoOrder>() {
+//            @Override
+//            public boolean filter(VideoOrder videoOrder) throws Exception {
+//                return videoOrder.getMoney() > 5;
+//            }
+//        }).setParallelism(3);
+//
+//        filterDS.print().setParallelism(4);
+//
+//        //DataStream需要调用execute,可以取个名称
+//        env.execute("source job");
+//    }
+//
+//}
--- Flink2/flink_course1/src/main/java/com/dxtd/flink/model/VideoOrder.java
+++ Flink2/flink_course1/src/main/java/com/dxtd/flink/model/VideoOrder.java
@@ -1,19 +1,24 @@
-package com.dxtd.flink.model;
+package  com.dxtd.flink.model;
 
+import com.dxtd.flink.util.TimeUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import java.util.Date;
 
-/**
- * 订单实体类
- *
- * */
+
+//订单实体类
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class VideoOrder {
+
     private String tradeNo;
+
     private String title;
+
     private int money;
 
 }
