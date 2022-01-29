26c22afe249b154b07688581c6f2479538de391c
cxinping <xpws2006@163.com>
2022-01-30 02:08:02 +0800

    no message

---
+++ Flink2/flink_course1/src/main/java/com/dxtd/flink/model/VideoOrder2.java
@@ -0,0 +1,32 @@
+package com.dxtd.flink.model;
+
+import com.dxtd.flink.util.TimeUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoOrder2 {
+    private String tradeNo;
+    private String title;
+    private int money;
+    private int userId;
+    private Date createTime;
+
+    @Override
+    public String toString() {
+        return "VideoOrder2{" +
+                "tradeNo='" + tradeNo + '\'' +
+                ", title='" + title + '\'' +
+                ", money=" + money +
+                ", userId=" + userId +
+                ", createTime=" + TimeUtil.format(createTime) +
+                '}';
+    }
+
+
+}
--- Flink2/flink_course1/src/main/java/com/dxtd/flink/source/VideoOrderSourceV2.java
+++ Flink2/flink_course1/src/main/java/com/dxtd/flink/source/VideoOrderSourceV2.java
@@ -1,89 +1,81 @@
-//package com.dxtd.flink.source;
-//
-//import com.dxtd.flink.model.VideoOrder;
-//import com.dxtd.flink.util.TimeUtil;
-//import org.apache.flink.configuration.Configuration;
-//import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
-//import java.util.*;
-//
-///**
-// * 小滴课堂,愿景：让技术不再难学
-// *
-// * @Description
-// * @Author 二当家小D
-// * @Remark 有问题直接联系我，源码-笔记-技术交流群
-// * @Version 1.0
-// **/
-//
-//public class VideoOrderSourceV2 extends RichParallelSourceFunction<VideoOrder> {
-//
-//
-//    private volatile Boolean flag = true;
-//
-//    private Random random = new Random();
-//
-//    private static List<VideoOrder> list = new ArrayList<>();
-//    static {
-//        list.add(new VideoOrder("","java",10,0,null));
-//        list.add(new VideoOrder("","spring boot",15,0,null));
-////        list.add(new VideoOrder("","springc loud",20,0,null));
-////        list.add(new VideoOrder("","flink",45,0,null));
-////        list.add(new VideoOrder("","面试专题第一季",50,0,null));
-////        list.add(new VideoOrder("","项目大课",1,0,null));
-////        list.add(new VideoOrder("","kafka",300,0,null));
-//    }
-//
-//
-//    /**
-//     * run 方法调用前 用于初始化连接
-//     * @param parameters
-//     * @throws Exception
-//     */
-//    @Override
-//    public void open(Configuration parameters) throws Exception {
-//        System.out.println("-----open-----");
-//    }
-//
-//    /**
-//     * 用于清理之前
-//     * @throws Exception
-//     */
-//    @Override
-//    public void close() throws Exception {
-//        System.out.println("-----close-----");
-//    }
-//
-//
-//    /**
-//     * 产生数据的逻辑
-//     * @param ctx
-//     * @throws Exception
-//     */
-//    @Override
-//    public void run(SourceContext<VideoOrder> ctx) throws Exception {
-//
-//        while (flag){
-//            Thread.sleep(1000);
-//            String id = UUID.randomUUID().toString().substring(30);
-//            int userId = random.nextInt(10);
-//            int videoNum = random.nextInt(list.size());
-//            VideoOrder videoOrder = list.get(videoNum);
-//            videoOrder.setUserId(userId);
-//            videoOrder.setCreateTime(new Date());
-//            videoOrder.setTradeNo(id);
-//            System.out.println("产生:"+videoOrder.getTitle()+"，价格:"+videoOrder.getMoney()+", 时间:"+ TimeUtil.format(videoOrder.getCreateTime()));
-//            ctx.collect(videoOrder);
-//        }
-//
-//
-//    }
-//
-//    /**
-//     * 控制任务取消
-//     */
-//    @Override
-//    public void cancel() {
-//
-//        flag = false;
-//    }
-//}
+package com.dxtd.flink.source;
+
+import com.dxtd.flink.model.VideoOrder2;
+import com.dxtd.flink.util.TimeUtil;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+import java.util.*;
+
+
+public class VideoOrderSourceV2 extends RichParallelSourceFunction<VideoOrder2> {
+
+
+    private volatile Boolean flag = true;
+
+    private Random random = new Random();
+
+    private static List<VideoOrder2> list = new ArrayList<>();
+    static {
+        list.add(new VideoOrder2("","java",10,0,null));
+        list.add(new VideoOrder2("","spring boot",15,0,null));
+//        list.add(new VideoOrder("","springc loud",20,0,null));
+//        list.add(new VideoOrder("","flink",45,0,null));
+//        list.add(new VideoOrder("","面试专题第一季",50,0,null));
+//        list.add(new VideoOrder("","项目大课",1,0,null));
+//        list.add(new VideoOrder("","kafka",300,0,null));
+    }
+
+
+    /**
+     * run 方法调用前 用于初始化连接
+     * @param parameters
+     * @throws Exception
+     */
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        System.out.println("-----open-----");
+    }
+
+    /**
+     * 用于清理之前
+     * @throws Exception
+     */
+    @Override
+    public void close() throws Exception {
+        System.out.println("-----close-----");
+    }
+
+
+    /**
+     * 产生数据的逻辑
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void run(SourceContext<VideoOrder2> ctx) throws Exception {
+
+        while (flag){
+            Thread.sleep(1000);
+            String id = UUID.randomUUID().toString().substring(30);
+            int userId = random.nextInt(10);
+            int videoNum = random.nextInt(list.size());
+            VideoOrder2 videoOrder = list.get(videoNum);
+            videoOrder.setUserId(userId);
+            videoOrder.setCreateTime(new Date());
+            videoOrder.setTradeNo(id);
+            System.out.println("产生:"+videoOrder.getTitle()+"，价格:"+videoOrder.getMoney()+", 时间:"+ TimeUtil.format(videoOrder.getCreateTime()));
+            ctx.collect(videoOrder);
+        }
+
+
+    }
+
+    /**
+     * 控制任务取消
+     */
+    @Override
+    public void cancel() {
+
+        flag = false;
+    }
+}
