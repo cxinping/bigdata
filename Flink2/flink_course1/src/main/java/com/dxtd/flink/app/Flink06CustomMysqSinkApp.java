d66874ab080fda403df10913b86cb65e35bdc2a8
cxinping <xpws2006@163.com>
2022-01-30 13:51:10 +0800

    no message

--- Flink2/flink_course1/src/main/java/com/dxtd/flink/app/Flink06CustomMysqSinkApp.java
+++ Flink2/flink_course1/src/main/java/com/dxtd/flink/app/Flink06CustomMysqSinkApp.java
@@ -9,14 +9,9 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * 小滴课堂,愿景：让技术不再难学
  *
- * @Description 流处理
- * @Author 二当家小D
- * @Remark 有问题直接联系我，源码-笔记-技术交流群
- * @Version 1.0
+ *
  **/
-
 public class Flink06CustomMysqSinkApp {
 
     /**
