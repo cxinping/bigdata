3725da32a54931014a926db72997a804300ca34f
cxinping <xpws2006@163.com>
2022-02-06 02:31:50 +0800

    no message

--- flink_study1/src/main/java/net/xdclass/app/TupleApp.java
+++ flink_study1/src/main/java/net/xdclass/app/TupleApp.java
@@ -9,12 +9,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * 小滴课堂,愿景：让技术不再难学
  *
- * @Description
- * @Author 二当家小D
- * @Remark 有问题直接联系我，源码-笔记-技术交流群
- * @Version 1.0
+ *
  **/
 
 public class TupleApp {
--- flink_study1/src/main/java/net/xdclass/app/WebUIApp.java
+++ flink_study1/src/main/java/net/xdclass/app/WebUIApp.java
@@ -7,12 +7,8 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
- * 小滴课堂,愿景：让技术不再难学
  *
- * @Description
- * @Author 二当家小D
- * @Remark 有问题直接联系我，源码-笔记-技术交流群
- * @Version 1.0
+ *
  **/
 
 public class WebUIApp {
