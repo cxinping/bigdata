34e9e2bffccc47e20f770d015a3e367a0150a296
t-peng.jiang <Jp1314@jp>
2022-04-08 08:30:32 +0800

    no message

--- go_projects/main.go
+++ go_projects/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var a = "aaa"
-	fmt.Println(a)
+	/* 创建切片 */
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	printSlice(numbers)
 
-	b := 1
-	fmt.Println(b)
+	/* 打印原始切片 */
+	fmt.Println("numbers ==", numbers)
+
+	/* 打印子切片从索引1(包含) 到索引4(不包含)*/
+	fmt.Println("numbers[1:4] ==", numbers[1:4])
+
 }
