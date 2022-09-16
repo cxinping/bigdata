eb2058411bd291e5e577aa97d2f29296fb2e8d8e
t-peng.jiang <Jp1314@jp>
2022-09-16 14:53:14 +0800

    no message

--- go_projects/test1.go
+++ go_projects/test1.go
@@ -1,9 +1,22 @@
 package main
 
-import (
-	"runtime"
-)
+import "fmt"
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+
+}
+
+func variableInitValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+
+	fmt.Println(a, b, s)
+}
 
 func main() {
-	print(runtime.GOARCH)
+	//variableZeroValue()
+	variableInitValue()
 }
