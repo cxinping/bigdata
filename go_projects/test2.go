7ac5f5804b74b03f3efa310255bc31c70f37bd3a
t-peng.jiang <Jp1314@jp>
2022-09-19 01:41:41 +0800

    no message

--- go_projects/test2.go
+++ go_projects/test2.go
@@ -2,15 +2,8 @@ package main
 
 import "fmt"
 
-type weapon int
-
-const (
-	ARROW weapon = iota
-	Shriken
-	Sniper
-)
-
 func main() {
+
 	fmt.Println("hello Go")
-	fmt.Println(ARROW, Shriken, Sniper)
+
 }
