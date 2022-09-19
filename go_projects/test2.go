22539575cff54e82251ccc34e1b03feca716bc90
t-peng.jiang <Jp1314@jp>
2022-09-19 15:07:24 +0800

    函数

--- go_projects/test2.go
+++ go_projects/test2.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 )
 
 func test1() {
 	fmt.Println("aaa")
 }
 
-func main() {
-	const filename = "abc.txt"
-	contents, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("%s\n", contents)
-	}
-}
+//func main() {
+//	const filename = "abc.txt"
+//	contents, err := ioutil.ReadFile(filename)
+//	if err != nil {
+//		fmt.Println(err)
+//	} else {
+//		fmt.Printf("%s\n", contents)
+//	}
+//}
--- go_projects/test3.go
+++ go_projects/test3.go
@@ -1,5 +1,23 @@
 package main
 
-//func main() {
-//	fmt.Println("abc")
-//}
+import "fmt"
+
+func main() {
+	var a int = 20 /* 声明实际变量 */
+	//var ip *int    /* 声明指针变量 */
+	var team [3]string
+	team[0] = "a"
+	team[1] = "b"
+	team[2] = "c"
+
+	//ip = &a /* 指针变量的存储地址 */
+
+	fmt.Printf("a 变量的地址是: %x\n", &a, &team, &team[0], &team[1])
+
+	///* 指针变量的存储地址 */
+	//fmt.Printf("ip 变量储存的指针地址: %x\n", ip)
+	//
+	///* 使用指针访问值 */
+	//fmt.Printf("*ip 变量的值: %d\n", *ip)
+
+}
