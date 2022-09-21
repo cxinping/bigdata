c5e2aa4a8ca85dddad398a78bdd3162be243b982
t-peng.jiang <Jp1314@jp>
2022-09-21 18:59:06 +0800

    容器

--- go_projects/test3.go
+++ go_projects/test3.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	//cache := make(map[string]string)
-	//cache["name"] = "wangwu"
-	//fmt.Println(cache["name"])
-
 	var arr1 [5]int
 	arr2 := [3]int{1, 3, 5}
 	arr3 := [...]int{2, 4, 6, 8, 10}
+	var grid [4][5]int
+
 	fmt.Println(arr1, arr2, arr3)
+	fmt.Println(grid)
 
 }
