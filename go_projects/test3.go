fb8f6dbd980621836c7c4ce3762a376edc7c3ca2
t-peng.jiang <Jp1314@jp>
2022-09-22 08:52:21 +0800

    切片

--- go_projects/test3.go
+++ go_projects/test3.go
@@ -2,24 +2,24 @@ package main
 
 import "fmt"
 
-func printArray(arr [5]int) {
+func printArray(arr *[5]int) {
 	for i, v := range arr {
 		fmt.Println(i, v, arr[i])
 	}
 }
 
-func main() {
-	var arr1 [5]int
-	arr2 := [3]int{1, 3, 5}
-	arr3 := [...]int{2, 4, 6, 8, 10}
-	var grid [4][5]int
-
-	fmt.Println(arr1, arr2, arr3)
-	fmt.Println(grid)
-
-	//for i, v := range arr3 {
-	//	fmt.Println(i, v, arr3[i])
-	//}
-
-	printArray(arr2)
-}
+//func main() {
+//	var arr1 [5]int
+//	arr2 := [3]int{1, 3, 5}
+//	arr3 := [...]int{2, 4, 6, 8, 10}
+//	var grid [4][5]int
+//
+//	fmt.Println(arr1, arr2, arr3)
+//	fmt.Println(grid)
+//
+//	//for i, v := range arr3 {
+//	//	fmt.Println(i, v, arr3[i])
+//	//}
+//
+//	printArray(&arr3)
+//}
