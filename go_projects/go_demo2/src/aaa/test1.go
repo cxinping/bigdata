84a6fed99d5d8a3c3f76cd169d909d0a61bec3ca
t-peng.jiang <Jp1314@jp>
2022-09-24 06:45:21 +0800

    no message

--- go_projects/go_demo2/src/aaa/test1.go
+++ go_projects/go_demo2/src/aaa/test1.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	//_ "github.com/go-sql-driver/mysql"
+)
 
 type Rectangle struct {
 	width, height float64
@@ -13,9 +17,25 @@ func demo1() {
 	fmt.Printf("i的地址： %p %p \n", &i, &j)
 }
 
+var (
+	userName  string = "xinping"
+	password  string = "123456"
+	ipAddrees string = "192.168.11.11"
+	port      int    = 3306
+	dbName    string = "codebaoku"
+	charset   string = "utf8"
+)
+
+func test_db() {
+	db, err := sql.Open("mysql", "xinping:123456@/codebaoku?charset=utf8")
+	fmt.Println(db, err)
+
+}
+
 func main() {
-	//fmt.Println("hello world")
+	//fmt.Println("hello world 222")
 
-	demo1()
+	//demo1()
 
+	test_db()
 }
