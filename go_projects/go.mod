1023de3545ec848527a7b3eef1eda14efcef6d44
t-peng.jiang <Jp1314@jp>
2022-09-23 10:55:43 +0800

    no message

--- go_projects/go.mod
+++ go_projects/go.mod
@@ -2,7 +2,3 @@ module ioaio.com/learngo
 
 go 1.16
 
-require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
-)
--- go_projects/zaptest.go
+++ go_projects/zaptest.go
@@ -1,13 +1,14 @@
 package main
 
-import (
-	"fmt"
-
-	"go.uber.org/zap"
-)
-
-func main() {
-	logger, _ := zap.NewProduction()
-	logger.Warn("warning test")
-	fmt.Println(logger)
-}
+//
+//import (
+//	"fmt"
+//
+//	"go.uber.org/zap"
+//)
+//
+//func main() {
+//	logger, _ := zap.NewProduction()
+//	logger.Warn("warning test")
+//	fmt.Println(logger)
+//}
