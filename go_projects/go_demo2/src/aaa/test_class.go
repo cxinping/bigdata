19076ae4c32d666e9c5b20323031622d5ff96ec3
t-peng.jiang <Jp1314@jp>
2022-09-26 03:34:33 +0800

    no message

--- go_projects/go_demo2/src/aaa/test_class.go
+++ go_projects/go_demo2/src/aaa/test_class.go
@@ -6,10 +6,16 @@ import (
 )
 
 func Test_struct1() {
+	//匿名函数
 	res := func(a, b float64) float64 {
 		return math.Pow(a, b)
 	}(2, 3)
 
 	fmt.Println("res=", res)
+
+	// 匿名类
+	addr := struct{ province, city string }{"陕西省", "西安市"}
+	fmt.Printf("addr匿名类的类型是 %p", addr, "\n----------\n")
+	fmt.Println(addr.province, addr.city)
 
 }
