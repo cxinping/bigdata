da4d46d980f9f9bdd9f62706ec264efe1c7e6ab1
t-peng.jiang <Jp1314@jp>
2022-09-29 19:28:55 +0800

    no message

--- go_projects/go_demo2/src/aaa/test_reflection.go
+++ go_projects/go_demo2/src/aaa/test_reflection.go
@@ -13,11 +13,14 @@ type RefPeople struct {
 func TestReflection1() {
 	fmt.Println("--- over ---")
 
-	//people := RefPeople{name: "wangwu", address: "beijing"}
+	//a := 1.5
+	//fmt.Println(reflect.TypeOf(a))
+	//fmt.Println(reflect.ValueOf(a))
+
+	people := RefPeople{name: "wangwu", address: "北京海淀"}
 	//fmt.Printf("RefPeople+> %T %V", people, &people)
 
-	a := 1.5
-	fmt.Println(reflect.TypeOf(a))
-	fmt.Println(reflect.ValueOf(a))
+	v := reflect.ValueOf(people)
+	fmt.Println(v.NumField(), v.FieldByIndex([]int{0}), v.FieldByIndex([]int{1}))
 
 }
