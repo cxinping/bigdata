e210833c6f475e4b8ba57ceebe396ba78478cbc9
t-peng.jiang <Jp1314@jp>
2022-09-30 13:43:32 +0800

    no message

--- go_projects/go_demo2/src/aaa/test_db.go
+++ go_projects/go_demo2/src/aaa/test_db.go
@@ -7,32 +7,29 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//var (
-//	userName  string = "xinping"
-//	password  string = "123456"
-//	ipAddrees string = "192.168.11.11"
-//	port      int    = 3306
-//	dbName    string = "codebaoku"
-//	charset   string = "utf8"
-//)
-
+// DbConn 定义数据库连接信息
 type DbConn struct {
 	Dsn string
 	Db  *sql.DB
 }
 
-func TestConnDB() {
-	//db, err := sql.Open("mysql", "xinping:123456@/codebaoku?charset=utf8")
-	//fmt.Println(db, err)
+// UserTable user_info表的映射对象
+type UserTable struct {
+	Uid        int
+	Username   string
+	Department string
+	Created    string
+}
 
+func TestConnDB() {
 	var err error
 	dbConn := DbConn{Dsn: "xinping:123456@/codebaoku?charset=utf8"}
 
-	fmt.Println(dbConn)
-	if err != nil {
-		panic(err)
-		return
-	}
+	fmt.Println(dbConn, err)
+	//if err != nil {
+	//	panic(err)
+	//	return
+	//}
 
 	//defer dbConn.Db.Close()
 
