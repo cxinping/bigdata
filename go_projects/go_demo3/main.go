10b2ab44c1d561544c8a2a12207beeb5df5a09b8
t-peng.jiang <Jp1314@jp>
2022-10-26 20:29:50 +0800

    test case 6

--- go_projects/go_demo3/main.go
+++ go_projects/go_demo3/main.go
@@ -6,18 +6,28 @@ import (
 	"runtime"
 )
 
-func test1() {
+func showCpuCores() {
 	cpuNums := runtime.NumCPU()
 	fmt.Println("CPU核心数: ", cpuNums)
 
 	var numCores = flag.Int("n", 2, "CPU核心数")
 	fmt.Println("CPU核心数: ", numCores)
 	runtime.GOMAXPROCS(*numCores)
+	//time.Sleep(5 * time.Second)
+}
 
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		runtime.Gosched()
+		fmt.Println(s)
+	}
 }
 
 func main() {
 	//channel.Test1()
-	test1()
+	//showCpuCores()
+
+	go say("world")
+	say("hello")
 
 }
