391e4767d6663061891da753120192523104db3b
t-peng.jiang <Jp1314@jp>
2022-10-26 18:11:42 +0800

    test case 6

--- go_projects/go_demo3/main.go
+++ go_projects/go_demo3/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"runtime"
 )
 
-func main() {
-	//channel.Test1()
+func test1() {
 	cpuNums := runtime.NumCPU()
 	fmt.Println("CPU核心数: ", cpuNums)
+
+	var numCores = flag.Int("n", 2, "CPU核心数")
+	fmt.Println("CPU核心数: ", numCores)
+	runtime.GOMAXPROCS(*numCores)
+
+}
+
+func main() {
+	//channel.Test1()
+	test1()
 
 }
